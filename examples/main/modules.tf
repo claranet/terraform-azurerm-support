d59fccfc95165dbb1896ba645e641c0b19dda439
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-12-29 17:31:48 +0100

    AZ-515: Update example

--- examples/main/modules.tf
+++ examples/main/modules.tf
@@ -41,6 +41,24 @@ module "azure_network_vnet" {
   vnet_cidr           = ["10.10.0.0/16"]
 }
 
+module "az_monitor" {
+  source  = "claranet/run-iaas/azurerm//modules/vm-monitoring"
+  version = "x.x.x"
+
+  client_name    = var.client_name
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  environment    = var.environment
+  stack          = var.stack
+
+  resource_group_name        = module.rg.resource_group_name
+  log_analytics_workspace_id = module.logs.log_analytics_workspace_id
+
+  extra_tags = {
+    foo = "bar"
+  }
+}
+
 resource "tls_private_key" "bastion" {
   algorithm = "RSA"
 }
@@ -74,6 +92,10 @@ module "support" {
   subnet_cidr_list = ["10.10.10.0/24"]
   #  support_dns_zone_name = var.support_dns_zone_name
 
+  # Diag/logs
   diagnostics_storage_account_name      = module.logs.logs_storage_account_name
-  diagnostics_storage_account_sas_token = module.logs.logs_storage_account_sas_token
+  diagnostics_storage_account_sas_token = null # used by legacy agent only
+  azure_monitor_data_collection_rule_id = module.az_monitor.data_collection_rule_id
+  log_analytics_workspace_guid          = module.logs.log_analytics_workspace_guid
+  log_analytics_workspace_key           = module.logs.log_analytics_workspace_primary_key
 }
